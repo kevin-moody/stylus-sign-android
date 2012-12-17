4315e7f71682fa9cf27440d4c0fb2c6b41e8351a
kevinmoody <kevinmood@gmail.com>
2012-12-17 16:28:08 -0500

    initial checkin

---
+++ kcm-stylus-draw/gen/com/example/kcm_stylus_draw/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.kcm_stylus_draw;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ kcm-stylus-draw/src/com/example/kcm_stylus_draw/SketchActivity.java
@@ -0,0 +1,13 @@
+package com.example.kcm_stylus_draw;
+
+import android.content.Context;
+import android.view.View;
+
+public class SketchActivity extends View {
+
+	public SketchActivity(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
